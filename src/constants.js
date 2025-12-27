9258f4fe48705ee53b5bfb3baa9d54b63d94ea02
Siddhesh Todi <siddheshtodi@gmail.com>
2025-12-27 13:45:36 +0530

    updated directories to be ignored if ran in root directory

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,34 @@ export const CODE_EXTENSIONS = new Set([
 ]);
 
 export const IGNORE_DIRS = new Set([
+  // Development & Build
   'node_modules', '.git', 'dist', 'build',
   'coverage', '.cache', 'public', 'vendor',
-  'venv', '.venv', 'env', '.env', '__pycache__'
+  'venv', '.venv', 'env', '.env', '__pycache__',
+  '.pytest_cache', '.mypy_cache',
+
+  // Package Managers & Configs
+  '.npm',          // NPM Cache
+  '.nvm',          // Node Version Manager
+  '.yarn',         // Yarn Cache
+  '.pnpm-store',   // pnpm Store
+  '.local',        // Linux User Data (Contains Trash!)
+  '.config',       // User Configs
+  '.cargo',        // Rust Cache
+  '.rustup',       // Rust Toolchain
+
+  // IDEs
+  '.vscode', '.vscode-insiders', '.idea', '.vscode-server',
+
+  // Windows System
+  'AppData', '$Recycle.Bin', 'System Volume Information',
+  'Program Files', 'Program Files (x86)', 'Windows', 'Recovery', 'Config.Msi',
+
+  // macOS System
+  '.Trash', '.Trashes', 'Library', 'Applications',
+
+  // Linux System
+  'proc', 'sys', 'dev', 'var', 'tmp', 'usr', 'bin', 'sbin', 'etc', 'snap', 'boot', 'lib', 'lib64', 'opt', 'srv', 'run'
 ]);
 
 export const GLOBAL_CONFIG_PATH = path.join(os.homedir(), '.sweeprrc');
